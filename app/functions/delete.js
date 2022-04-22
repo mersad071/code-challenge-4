0a62697ed325410da2bceba37c18025a242539a8
Mersad Hadzic <mersad@bind.ba>
2022-04-22 23:50:50 +0200

    delete item

--- app/functions/delete.js
+++ app/functions/delete.js
@@ -1,5 +1,30 @@
+const { deleteItemModel } = require('../schema/item');
+const { DynamoDB } = require('aws-sdk');
+const ddb = new DynamoDB.DocumentClient();
+
 const handler = async event => {
-  return true;
+  try {
+    const { id } = event.arguments.input;
+    const Item = {
+      id
+    }
+    await deleteItemModel.validateAsync(Item);
+    
+    const { Attributes } = await ddb.delete({
+      TableName: process.env.DYNAMODB_TABLE,
+      Key: {
+        PK: `item`,
+        SK: `item#${id}`,
+      },
+      ReturnValues: "ALL_OLD",
+    });
+
+    return Attributes;
+    
+  } catch(err) {
+    console.log(err);
+    return err;
+  }
 }
 
 module.exports = { handler }
