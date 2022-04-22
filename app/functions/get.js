7919a8ee67ce1ab604e7df75af4e71b3bdab8bcc
Mersad Hadzic <mersad@bind.ba>
2022-04-23 00:08:31 +0200

    get item

--- app/functions/get.js
+++ app/functions/get.js
@@ -1,5 +1,27 @@
+const { deleteItemModel } = require('../schema/item');
+const { DynamoDB } = require('aws-sdk');
+const ddb = new DynamoDB.DocumentClient();
+
 const handler = async event => {
-  return true;
+  try {
+    const { id } = event.arguments.input;
+
+    await deleteItemModel.validateAsync({ id });
+    
+    const { Item } = await ddb.get({
+      TableName: process.env.DYNAMODB_TABLE,
+      Key: {
+        PK: `item`,
+        SK: `item#${id}`,
+      },
+    }).promise();
+
+    return Item;
+    
+  } catch(err) {
+    console.log(err);
+    return err;
+  }
 }
 
 module.exports = { handler }
