04baa0223e6ac30b410f35beb51453d17a776f78
Mersad Hadzic <mersad@bind.ba>
2022-04-25 13:36:52 +0200

    add signup

--- cc4-frontend/src/components/signup.js
+++ cc4-frontend/src/components/signup.js
@@ -1,4 +1,4 @@
-import { Input, FormControl, InputLabel, Button } from '@mui/material';
+import { Input, FormControl, InputLabel, Button, Grid, Card, CardContent } from '@mui/material';
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import Context from './context';
@@ -17,20 +17,32 @@ export default function Signup() {
   }
 
   return (
-    <form onSubmit={handleSubmit}>
-      <FormControl>
-        <InputLabel htmlFor="email">Email</InputLabel>
-        <Input type='email' id="email" name="email" />
-      </FormControl>
-      <FormControl>
-        <InputLabel htmlFor="password">Password</InputLabel>
-        <Input type='password' id="password" name="password" />
-      </FormControl>
-      <FormControl fullWidth={true}>
-        <Button type='submit' color='primary' variant='contained'>
-          Signup
-        </Button>
-      </FormControl>
-    </form>
+    <Grid container spacing={0} alignItems="center" justifyContent="center">
+      <Grid item xs={6} md={4}>
+        <Card sx={{ minWidth: 275, marginTop: '1em' }}>
+          <CardContent>
+            <h3>Sign In</h3>
+            <p className='text-muted'>Code challenge 4</p>
+            <form onSubmit={handleSubmit}>
+              <FormControl fullWidth={true} margin='normal'>
+                <InputLabel htmlFor="email">Email</InputLabel>
+                <Input type='email' id="email" name="email" />
+              </FormControl>
+
+              <FormControl fullWidth={true} margin='normal'>
+                <InputLabel htmlFor="password">Password</InputLabel>
+                <Input type='password' id="password" name="password" />
+              </FormControl>
+
+              <FormControl fullWidth={true}>
+                <Button type='submit' color='primary' variant='contained'>
+                  Signup
+                </Button>
+              </FormControl>
+            </form>
+          </CardContent>
+        </Card>
+      </Grid>
+    </Grid>
   );
 }
