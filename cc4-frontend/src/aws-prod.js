8e49528d507841af58e0752712b223a282fbffeb
Mersad Hadzic <mersad@bind.ba>
2022-04-25 20:24:09 +0200

    add dev and prod amplify config

---
+++ cc4-frontend/src/aws-prod.js
@@ -0,0 +1,9 @@
+const aws = {
+  aws_project_region: 'us-east-1',
+  aws_cognito_region: 'us-east-1',
+  aws_user_pools_id: 'us-east-1_44qqTrPcO',
+  aws_user_pools_web_client_id: '2krvj458271b3v0evll65ide65',
+  aws_appsync_authenticationType: 'AMAZON_COGNITO_USER_POOLS',
+  aws_appsync_graphqlEndpoint: 'https://4epifcsiqfdmfpwdgda7l7usxu.appsync-api.us-east-1.amazonaws.com/graphql',
+}
+export default aws;
