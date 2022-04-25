a7e4a27b0532a7aa82cfe435f87e6b84a318ec57
Mersad Hadzic <mersad@bind.ba>
2022-04-25 12:32:29 +0200

    add theme

--- cc4-frontend/src/App.js
+++ cc4-frontend/src/App.js
@@ -4,26 +4,30 @@ import Signin from './components/signin';
 import Signup from './components/signup';
 import { ContextProvider } from './components/context';
 import AuthGuard from './auth-guard';
+import { ThemeProvider } from '@emotion/react';
+import darkTheme from './ui/theme';
 
 function App() {
   return (
     <ContextProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route exact path="/" element={
-            <AuthGuard>
-              <Dashboard />
-            </AuthGuard>
-          } />
-          <Route exact path="/dashboard" element={
-            <AuthGuard>
-              <Dashboard />
-            </AuthGuard>
-          } />
-          <Route exact path="/signin" element={<Signin />} />
-          <Route exact path="/signup" element={<Signup />} />
-        </Routes>
-      </BrowserRouter>
+      <ThemeProvider theme={darkTheme}>
+        <BrowserRouter>
+          <Routes>
+            <Route exact path="/" element={
+              <AuthGuard>
+                <Dashboard />
+              </AuthGuard>
+            } />
+            <Route exact path="/dashboard" element={
+              <AuthGuard>
+                <Dashboard />
+              </AuthGuard>
+            } />
+            <Route exact path="/signin" element={<Signin />} />
+            <Route exact path="/signup" element={<Signup />} />
+          </Routes>
+        </BrowserRouter>
+      </ThemeProvider>
     </ContextProvider>
   )
 }
