4dd39ce62a3153bfe7c0a1d725b7863aa7e59880
Mersad Hadzic <mersad@bind.ba>
2022-04-25 11:44:32 +0200

    add routes

--- cc4-frontend/src/App.js
+++ cc4-frontend/src/App.js
@@ -1,8 +1,22 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Dashboard from './components/dashboard';
+import Signin from './components/signin';
+import Signup from './components/signup';
+
 function App() {
-  return (
-    <>
-      app
-    </>
+  return (     
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/" element={
+          <Dashboard />
+        } />
+        <Route exact path="/dashboard" element={
+          <Dashboard />
+        } />
+        <Route exact path="/signin" element={<Signin />} />
+        <Route exact path="/signup" element={<Signup />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
